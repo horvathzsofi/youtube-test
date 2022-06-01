94323f461ffa8916a8ed26b8ae1ec551cf1e248a
Zsófia Horváth <71212886+horvathzsofi@users.noreply.github.com>
2022-06-01 21:10:23 +0200

    Update LogIn.java

--- src/main/java/LogIn.java
+++ src/main/java/LogIn.java
@@ -1,18 +1,18 @@
 import java.util.Map;
 
 public class LogIn {
-    //private final String email = System.getenv("LOGIN_PART_ONE");
-    //private final String password = System.getenv("LOGIN_PART_TWO");
+    private final String email = System.getenv("LOGIN_PART_ONE");
+    private final String password = System.getenv("LOGIN_PART_TWO");
 
     public String getEmail() {
-        Map<String,String> env = System.getenv();
-        return env.get("LOGIN_PART_ONE");
-      //  return email;
+    //    Map<String,String> env = System.getenv();
+      //  return env.get("LOGIN_PART_ONE");
+        return email;
     }
 
     public String getPassword() {
-        Map<String,String> env = System.getenv();
-        return env.get("LOGIN_PART_TWO");
-        //return password;
+      //  Map<String,String> env = System.getenv();
+      //  return env.get("LOGIN_PART_TWO");
+        return password;
     }
 }
