69ce7c922b8b335fb3c253648fa4c696ed8534f1
horvathzsofi <71212886+horvathzsofi@users.noreply.github.com>
2022-06-01 20:12:53 +0200

    write test to get credentials from environment with dummy data

---
+++ src/test/java/LogInTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class LogInTest {
+    private LogIn logIn = null;
+
+    @BeforeEach
+    public void SetUp(){
+        LogIn logIn = new LogIn();
+    }
+
+    @Test
+    public void TestGetEmail(){
+        String email = logIn.getEmail();
+        Assertions.assertEquals("sajt", email);
+    }
+    @Test
+    public void TestGetPassword(){
+        String password = logIn.getPassword();
+        Assertions.assertEquals("alma", password);
+    }
+}
