8db9034a34df86e3bb7a5fe3acfdf66cc16919e9
horvathzsofi <71212886+horvathzsofi@users.noreply.github.com>
2022-06-01 21:24:27 +0200

    modified login once again T.T

--- src/main/java/LogIn.java
+++ src/main/java/LogIn.java
@@ -1,14 +1,36 @@
 import java.util.Map;
 
 public class LogIn {
-    private final String email = System.getenv("LOGIN_PART_ONE");
-    private final String password = System.getenv("LOGIN_PART_TWO");
+    private String email = "";
+    private String password = "";
+
+    private void getEnvironmentVariables(){
+        Map<String, String> env = System.getenv();
+        setEmail(env.get("LOGIN_PART_ONE"));
+        setPassword(env.get("LOGIN_PART_TWO"));
+    }
+
+    private void setEmail(String email) {
+        this.email = email;
+    }
+
+    private void setPassword(String password) {
+        this.password = password;
+    }
+
 
     public String getEmail() {
+        //Map<String,String> env = System.getenv();
+        //return env.get("LOGIN_PART_ONE");
         return email;
     }
 
     public String getPassword() {
+        //Map<String,String> env = System.getenv();
+        //return env.get("LOGIN_PART_TWO");
+
         return password;
     }
+
+    //"LOGIN_PART_TWO"
 }
