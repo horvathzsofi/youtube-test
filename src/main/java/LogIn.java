b2d32026ccb8a2a87ae0aa483e44185c37b3994a
horvathzsofi <71212886+horvathzsofi@users.noreply.github.com>
2022-06-01 21:32:25 +0200

    plz work now

--- src/main/java/LogIn.java
+++ src/main/java/LogIn.java
@@ -4,20 +4,15 @@ public class LogIn {
     private String email = "";
     private String password = "";
 
+    public LogIn() {
+        getEnvironmentVariables();
+    }
+
     private void getEnvironmentVariables() {
         Map<String, String> env = System.getenv();
-        setEmail(env.get("LOGIN_PART_ONE"));
-        setPassword(env.get("LOGIN_PART_TWO"));
+        email = env.get("LOGIN_PART_ONE");
+        password = env.get("LOGIN_PART_TWO");
     }
-
-    private void setEmail(String email) {
-        this.email = email;
-    }
-
-    private void setPassword(String password) {
-        this.password = password;
-    }
-
 
     public String getEmail() {
         return email;
